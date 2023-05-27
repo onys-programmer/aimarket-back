1e34675957f27c6e9cd05c4477948415ecf88e31
taehokim-onys <taeho.kim@timeblocks.com>
2023-05-27 15:38:12 +0900

    app js error fix cuz of send head twice

--- app.js
+++ app.js
@@ -35,8 +35,7 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.status(error.code || 500).json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
