b6eae583dc01acfa65ea28220e47622e7609e202
onys-programmer <thkim2017@gmail.com>
2023-05-31 03:07:49 +0900

    edit port as env

--- app.js
+++ app.js
@@ -91,8 +91,9 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((err) => {
