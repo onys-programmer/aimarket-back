2080ce3a07daf56f874d00eeb48676b840f971db
onys-programmer <thkim2017@gmail.com>
2023-06-07 20:05:18 +0900

    response 401 at check auth when expired

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,29 +1,32 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const HttpError = require("../models/http-error");
+const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
-  console.log("check auth is running");
+  console.log('check auth is running');
   try {
-    // console.log(req.headers);
-    const token = req.headers.authorization.split(" ")[1];
-    // console.log('token', token);
+    const token = req.headers.authorization.split(' ')[1];
 
     if (!token) {
-      console.log("Authentication failed! token is null");
-      throw new HttpError("Authentication failed! token is null", 401);
+      console.log('Authentication failed! token is null');
+      throw new HttpError('Authentication failed! token is null', 401);
     }
-    // console.log('token', token);
+
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    // console.log('decodedToken', decodedToken);
     req.userData = { userId: decodedToken.userId };
-    // console.log('check auth', req.userData);
-    console.log("check auth successed");
+
+    console.log('check auth successed');
     next();
   } catch (err) {
-    console.log("Authentication failed!: ", err);
-    const error = new HttpError(`Authentication failed!: ${err}`, 401);
-    return next(error);
+    console.log('Authentication failed!: ', err);
+
+    if (err.name === 'TokenExpiredError') {
+      // 토큰이 만료된 경우 401 Unauthorized 응답 전송
+      return res.status(401).json({ message: '토큰이 만료되었습니다.' });
+    }
+
+    // 다른 유효하지 않은 토큰인 경우 400 Bad Request 등의 에러 전송
+    return res.status(400).json({ message: '유효하지 않은 토큰입니다.' });
   }
 };
